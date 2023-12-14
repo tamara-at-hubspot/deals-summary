a3bdb6a5c2242bea600cc394d87873d964cd2341
Tamara Yu <tyu@hubspot.com>
2023-12-14 14:34:20 -0500

    Add dealstage filter

--- src/app/extensions/DealsSummary.tsx
+++ src/app/extensions/DealsSummary.tsx
@@ -1,12 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import {
   Alert,
+  Flex,
   LoadingSpinner,
+  MultiSelect,
+  type MultiSelectProps,
   Statistics,
   StatisticsItem,
 } from '@hubspot/ui-extensions';
 import { hubspot } from '@hubspot/ui-extensions';
-import { calculateTotalAmount } from './utils';
+import {
+  calculateTotalAmount,
+  constructStageOptions,
+  calculatePreselectedStages,
+} from './utils';
+
+type Options = MultiSelectProps['options'];
 
 // Define the extension to be run within the Hubspot CRM
 hubspot.extend(() => <DealsSummary />);
@@ -15,8 +24,11 @@ hubspot.extend(() => <DealsSummary />);
 const DealsSummary = () => {
   const [loading, setLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
+  const [deals, setDeals] = useState<any[]>([]);
   const [dealCount, setDealCount] = useState(0);
   const [totalAmount, setTotalAmount] = useState(0);
+  const [stageOptions, setStageOptions] = useState<Options>([]);
+  const [selectedStages, setSelectedStages] = useState<string[]>([]);
 
   useEffect(() => {
     // Request statistics data from serverless function
@@ -25,6 +37,11 @@ const DealsSummary = () => {
         propertiesToSend: ['hs_object_id'],
       })
       .then((deals) => {
+        setDeals(deals);
+        // deal stages available, all selected initially
+        setStageOptions(constructStageOptions(deals));
+        setSelectedStages(calculatePreselectedStages(deals));
+        // calculate stats
         setDealCount(deals.length);
         setTotalAmount(calculateTotalAmount(deals));
       })
@@ -35,6 +52,15 @@ const DealsSummary = () => {
         setLoading(false);
       });
   }, []);
+
+  const updateSelectedStages = (dealstages) => {
+    const filteredDeals = deals.filter((deal) =>
+      dealstages.includes(deal.properties.dealstage)
+    );
+    setDealCount(filteredDeals.length);
+    setTotalAmount(calculateTotalAmount(filteredDeals));
+    setSelectedStages(dealstages);
+  };
 
   if (loading) {
     // If loading, show a spinner
@@ -49,9 +75,18 @@ const DealsSummary = () => {
     );
   }
   return (
-    <Statistics>
-      <StatisticsItem label="Total deals" number={dealCount} />
-      <StatisticsItem label="Total amount" number={totalAmount} />
-    </Statistics>
+    <Flex direction="column" gap="medium">
+      <MultiSelect
+        value={selectedStages}
+        label="Select deal stages"
+        name="dealStages"
+        onChange={updateSelectedStages}
+        options={stageOptions}
+      />
+      <Statistics>
+        <StatisticsItem label="Total deals" number={dealCount} />
+        <StatisticsItem label="Total amount" number={totalAmount} />
+      </Statistics>
+    </Flex>
   );
 };
