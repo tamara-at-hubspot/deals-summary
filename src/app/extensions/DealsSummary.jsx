59879df0db066827e8dd8d3ed6dfb6e7e71c24e7
Tamara Yu <tyu@hubspot.com>
2023-12-13 21:57:51 -0500

    Use hubspot.serverless

--- src/app/extensions/DealsSummary.jsx
+++ src/app/extensions/DealsSummary.jsx
@@ -4,17 +4,16 @@ import {
   LoadingSpinner,
   Statistics,
   StatisticsItem,
-  Text,
 } from '@hubspot/ui-extensions';
 import { hubspot } from '@hubspot/ui-extensions';
 
 // Define the extension to be run within the Hubspot CRM
-hubspot.extend(({ runServerlessFunction }) => (
-  <DealsSummary runServerless={runServerlessFunction} />
+hubspot.extend(() => (
+  <DealsSummary />
 ));
 
 // Define the Extension component, taking in runServerless prop
-const DealsSummary = ({ runServerless }) => {
+const DealsSummary = () => {
   const [loading, setLoading] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
   const [dealsCount, setDealsCount] = useState(0);
@@ -22,18 +21,12 @@ const DealsSummary = ({ runServerless }) => {
 
   useEffect(() => {
     // Request statistics data from serverless function
-    runServerless({
-      name: 'get-data',
+    hubspot.serverless('get-data', {
       propertiesToSend: ['hs_object_id'],
     })
-      .then((serverlessResponse) => {
-        if (serverlessResponse.status == 'SUCCESS') {
-          const { response } = serverlessResponse;
-          setDealsCount(response.dealsCount);
-          setTotalAmount(response.totalAmount);
-        } else {
-          setErrorMessage(serverlessResponse.message);
-        }
+      .then((response) => {
+        setDealsCount(response.dealsCount);
+        setTotalAmount(response.totalAmount);
       })
       .catch((error) => {
         setErrorMessage(error.message);
@@ -41,7 +34,7 @@ const DealsSummary = ({ runServerless }) => {
       .finally(() => {
         setLoading(false);
       });
-  }, [runServerless]);
+  }, []);
 
   if (loading) {
     // If loading, show a spinner
@@ -57,12 +50,8 @@ const DealsSummary = ({ runServerless }) => {
   }
   return (
     <Statistics>
-      <StatisticsItem label="Open deals" number={dealsCount}>
-        <Text>Total number of deals contact is associated with</Text>
-      </StatisticsItem>
-      <StatisticsItem label="Unit price" number={totalAmount}>
-        <Text>High End</Text>
-      </StatisticsItem>
+      <StatisticsItem label="Total deals" number={dealsCount}/>
+      <StatisticsItem label="Total amount" number={totalAmount}/>
     </Statistics>
   );
 };
